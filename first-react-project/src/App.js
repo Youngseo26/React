218975f5d30c3cb42b7924e91f0be7317ebefe27
김영서 <dudtj926@naver.com>
2024-12-22 13:56:20 +0900

    payment form input에 입력하고 console에 찍기
    payment form input에 입력하고 console에 찍기

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -1,28 +1,30 @@
 import "./App.css";
-import Toast from "./components/Toast";
+import NewPayment from "./components/NewPayment/NewPayment";
+//import Toast from "./components/Toast";
 
 function App() {
-  const messageArray = [
-    {
-      title: "success",
-      text: "Right On! Your account has been updated.",
-    },
-    {
-      title: "warning",
-      text: "Hmmm. Something doesn't look right.",
-    },
-    {
-      title: "error",
-      text: "Un oh! Something went terribly wrong!",
-    },
-  ];
+  // const messageArray = [
+  //   {
+  //     title: "success",
+  //     text: "Right On! Your account has been updated.",
+  //   },
+  //   {
+  //     title: "warning",
+  //     text: "Hmmm. Something doesn't look right.",
+  //   },
+  //   {
+  //     title: "error",
+  //     text: "Un oh! Something went terribly wrong!",
+  //   },
+  // ];
 
   return (
-    <div className="wrapper">
-      {messageArray.map((message) => (
-        <Toast message={message} />
-      ))}
-    </div>
+    // <div className="wrapper">
+    //   {messageArray.map((message) => (
+    //     <Toast message={message} />
+    //   ))}
+    // </div>
+    <NewPayment />
   );
 }
 
---
+++ first-react-project/src/components/NewPayment/PaymentForm.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+import "./PaymentForm.css";
+
+const PaymentForm = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [today, setToday] = useState(null);
+
+  const inputTypeTextChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+  const inputTypeNumberChangeHandler = (event) => {
+    setPrice(event.target.value);
+  };
+  const inputTypeDateChangeHandler = (event) => {
+    setToday(event.target.value);
+  };
+
+  const buttonSubmitHandler = () => {
+    console.log("name", name);
+    console.log("price", price);
+    console.log("today", today);
+  };
+  return (
+    <form>
+      <div className="new-payment__controls">
+        <div className="new-payment__control">
+          <label>이름</label>
+          <input
+            type="text"
+            value={name}
+            onChange={inputTypeTextChangeHandler}
+          />
+        </div>
+        <div className="new-payment__control">
+          <label>금액</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={price}
+            onChange={inputTypeNumberChangeHandler}
+          />
+        </div>
+        <div className="new-payment__control">
+          <label>날짜</label>
+          <input
+            type="date"
+            min="1990-01-01"
+            max="2024-12-31"
+            value={today}
+            onChange={inputTypeDateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-payment__actions">
+        <button type="button" onClick={buttonSubmitHandler}>
+          결제 추가
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default PaymentForm;
