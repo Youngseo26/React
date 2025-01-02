a227f2af9beda1b49c9dd298bf9076b1f972e2bf
김영서 <dudtj926@naver.com>
2025-01-02 18:25:40 +0900

    fix : input value 없을때 배경 바꾸기 css 클래스이용_2
    fix : input value 없을때 배경 바꾸기 css 클래스이용_2

--- first-react-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ first-react-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { styled } from "react";
 
 import Button from "../../UI/Button";
 import "./CourseInput.css";
@@ -19,52 +18,24 @@ const CourseInput = (props) => {
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    //겂이 ''인 경우, 배경 빨간색으로
+    //값이 ''인 경우, 배경 빨간색으로
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
   };
+  console.log(isValid);
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl>
-        <FormControlLabel>목표</FormControlLabel>
-        <FormControlInput
-          type="text"
-          onChange={goalInputChangeHandler}
-          isValid={isValid}
-        />
-      </FormControl>
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label>목표</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
   );
 };
 
 export default CourseInput;
-
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-`;
-
-const FormControlLabel = styled.label`
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-`;
-
-const FormControlInput = styled.input`
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-  ${(props) =>
-    !props.isValid &&
-    `
-    background-color: salmon;
-  border-color: red;
-    `}
-`;
