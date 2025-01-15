d6cb8f58e257c7be861831b0f0682e93a9d73aef
김영서 <dudtj926@naver.com>
2025-01-15 15:50:49 +0900

    async, await
    async, await

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -1,24 +1,32 @@
-import React, { useContext } from "react";
-
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./context/AuthContext";
+import React from "react";
+import NewsList from "./components/NewsList";
 
 function App() {
-  const context = useContext(AuthContext);
-  return (
-    <>
-      <MainHeader />
-      <main>
-        {!context.isLoggedIn && <Login />}
-        {context.isLoggedIn && <Home />}
-      </main>
-    </>
-  );
+  return <NewsList />;
 }
 
 export default App;
+// import React, { useContext } from "react";
+
+// import Login from "./components/Login/Login";
+// import Home from "./components/Home/Home";
+// import MainHeader from "./components/MainHeader/MainHeader";
+// import AuthContext from "./context/AuthContext";
+
+// function App() {
+//   const context = useContext(AuthContext);
+//   return (
+//     <>
+//       <MainHeader />
+//       <main>
+//         {!context.isLoggedIn && <Login />}
+//         {context.isLoggedIn && <Home />}
+//       </main>
+//     </>
+//   );
+// }
+
+// export default App;
 
 // import React, { useState } from "react";
 
