1394f6eeca9ab4b3d103cef44d1b93e038ad73b7
김영서 <dudtj926@naver.com>
2025-01-21 20:29:06 +0900

    여러개의 state로 counter App
    여러개의 state로 counter App

--- first-react-project/src/components/Counter.js
+++ first-react-project/src/components/Counter.js
@@ -2,7 +2,9 @@ import classes from "./Counter.module.css";
 import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
@@ -13,13 +15,15 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
+
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>토글 카운터</button>
       <button onClick={incrementHandler}>숫자 증가</button>
       <button onClick={increase10Handler}>숫자 10 증가</button>
--- first-react-project/src/store/index.js
+++ first-react-project/src/store/index.js
@@ -1,19 +1,32 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
   if (action.type === "increment") {
     return {
+      ...state,
       counter: state.counter + 1,
     };
   }
 
   if (action.type === "decrement") {
     return {
+      ...state,
       counter: state.counter - 1,
     };
   }
   if (action.type === "increase") {
-    return { counter: state.counter + action.amount };
+    return {
+      ...state,
+      counter: state.counter + action.amount,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      //counter: state.counter, 도 되지만 스프레드 연산자 사용하고 바뀌는 값만 명시
+      ...state,
+      showCounter: !state.showCounter,
+    };
   }
   return state;
 };
