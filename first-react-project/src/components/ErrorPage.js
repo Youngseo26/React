d8145c1e7f641762e84efcae8e296266d2a3b1a2
김영서 <dudtj926@naver.com>
2025-01-22 21:12:24 +0900

    react-router-dom practice
    react-router-dom practice

---
+++ first-react-project/src/components/ErrorPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return <div>This is an error Page!!</div>;
+};
+
+export default ErrorPage;
--- first-react-project/src/components/UI/DummyText.js
+++
@@ -1,9 +0,0 @@
-import React, { memo } from "react";
-
-const DummyText = (props) => {
-  props.func();
-  console.log("Child Component rendered!");
-  return <p>dummy</p>;
-};
-
-export default memo(DummyText);
