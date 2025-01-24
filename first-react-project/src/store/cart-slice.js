010931615ecfd38436aa12c217a67cb1de76219c
김영서 <dudtj926@naver.com>
2025-01-24 20:53:32 +0900

    redux async thunk이용해서 장바구니 새로고침해도 안 사라지게
    redux async thunk이용해서 장바구니 새로고침해도 안 사라지게

--- first-react-project/src/store/cart-slice.js
+++ first-react-project/src/store/cart-slice.js
@@ -1,5 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://redux-async-5eeaf-default-rtdb.firebaseio.com/cart.json"
+      );
+      if (!response.ok) {
+        throw new Error("Fail!");
+      }
+      const data = await response.json();
+      return data;
+    };
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      //에러 처리
+      console.error(error.message);
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://redux-async-5eeaf-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Fail!");
+      }
+    };
+    try {
+      await sendRequest();
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+};
+
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
@@ -7,8 +51,11 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
-      /*TODO- 여기에 코드를 작성해 주세요*/
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
@@ -26,7 +73,6 @@ const cartSlice = createSlice({
       }
     },
     removeItemFromCart(state, action) {
-      /*TODO- 여기에 코드를 작성해 주세요*/
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
