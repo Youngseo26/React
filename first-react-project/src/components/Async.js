6b9aaaf0252cdaf4b0a905f5ca3c6022d9c0b35a
김영서 <dudtj926@naver.com>
2025-01-27 21:48:31 +0900

    newAPI 토큰값 제외
    newAPI 토큰값 제외

--- first-react-project/src/components/Async.js
+++ first-react-project/src/components/Async.js
@@ -3,9 +3,7 @@ import React, { useEffect, useState } from "react";
 const Async = () => {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0f9a604e9f4342949b8b30e54c90fa1f"
-    )
+    fetch("newApiToken")
       .then((res) => res.json())
       .then((data) => setArticles(data.articles));
   }, []);
